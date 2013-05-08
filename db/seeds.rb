b4af05b3c35eaa6430c5f65717a060f502429679
Neil Volungis <nvolungis@ctpboston.com>
2013-05-08 09:30:59 -0400

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 mission = {:body => 'body copy', :statement => 'statement'}
 
-#Mission.create(mission)
+Mission.create(mission)
 
 thework = {:body => "Less of an outlet and more of an adhesive, HUM intends to bond together artists and writers from diverse backgrounds and influences.  Here are a few artists that we'll feature in Issue  (I):"}
 
-#Thework.destroy_all
-#Thework.create(thework)
+Thework.destroy_all
+Thework.create(thework)
 
 artists = [
   {:name => 'Anna Leocha', :info => 'Filmmaker, Writer  / 23 / Jamaica Plain', :about => 'Lorem ipsum dolor sit amet, cum repudiare pertinacia et, omnis solum honestatis an vis. Mea admodum molestie percipitur in, ex his lucilius sapientem elaboraret.'},
