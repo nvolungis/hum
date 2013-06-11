4aef1adea7360dc9f24ed3823fa74a7cfb676602
Neil Volungis <nvolungis@ctpboston.com>
2013-06-11 18:49:08 -0400

    db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
 
-mission = {:body => "HUM journal is a publication designed to share and archive the creative work of artists of all mediums in the Greater Boston area.  A small print publication that expands on its content and impact through an internet presence, we hope HUM will further facilitate and expose a culture of beauty-production and creative camaraderie in Boston.  
-
-Each issue will pose a theme that can be interpreted as abstractly or pointedly as the artist wishes.  The collection will be curated by Anna and Holly.  Issue (I) will ask its contributors to reflect on “Boston.”  It being  a particularly provocative moment for Boston’s arts culture, we’re asking: ", :statement => 'What does it mean to be an artist in Boston? '}
+mission = {:body => "HUM journal is a publication designed to share and archive the creative work of artists of all mediums in the Greater Boston area.  A small print publication that expands on its content and impact through an internet presence, we hope HUM will further facilitate and expose a culture of beauty-production and creative camaraderie in Boston. Each issue will pose a theme that can be interpreted as abstractly or pointedly as the artist wishes.  The collection will be curated by Anna and Holly.  Issue (I) will ask its contributors to reflect on “Boston.”  It being  a particularly provocative moment for Boston’s arts culture, we’re asking: ", :statement => 'What does it mean to be an artist in Boston? '}
 
 Mission.destroy_all
 Mission.create(mission)
