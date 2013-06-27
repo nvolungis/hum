b0b083bf35c17f9c886d786d831d775da8717449
Neil <nvolungis@gmail.com>
2013-06-27 01:10:06 -0400

    video

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,9 +3,10 @@
 	
 	  $('.workitem-image').viewer();
 	  $('#artists .artist').workitems();
-	  $('#header').fixbar({
-  	  spacer_element: '#strip'
-	  });
+	  $('#header').fixbar({ spacer_element: '#strip' });
 	  $('body').slideto();
+	  $('body').backToTop({
+  	  content: '<video loop="loop" autoplay="autoplay"><source src="/videos/hum-fur.mp4"></source><source src="/videos/hum-fur.webm"></source></video><span>TOP</span>'
+	  });
 	});
 }(jQuery));
